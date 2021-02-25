b801478a6768741f95cccab01f9e012fda97dc73
hy5424 <1031353743@qq.com>
2021-02-25 22:12:56 +0800

    init

--- util/excelUtil.py
+++ util/excelUtil.py
@@ -3,8 +3,6 @@
 
 import pandas as pd
 
-from util.OsUtil import OsUtil
-
 
 class ExcelUtil(object):
 
@@ -12,18 +10,3 @@ class ExcelUtil(object):
     def open_excel(excelUrl, sheetName):
         df = pd.read_excel(excelUrl, sheet_name=sheetName)
         return df
-
-    """
-    中华工单记录合并方法
-    """
-
-    def zhonghua_excel(inputDirUrl, outDirUrl):
-        files = OsUtil.walkFile(inputDirUrl)
-        list = []
-        for f in files:
-            f = f.replace('\\', '//')
-            excelData = ExcelUtil.open_excel(f, 'Sheet1')
-            excelData.drop(excelData[pd.isna(excelData['处理类型'])].index, inplace=True)
-            list.append(excelData)
-        df = pd.concat(list)
-        df.to_excel(outDirUrl + 'output.xlsx', index=False, header=True)
