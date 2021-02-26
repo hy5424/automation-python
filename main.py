de607f7642b71c6b27deadc2d7a8a917f176de9d
hy5424 <1031353743@qq.com>
2021-02-26 17:50:04 +0800

    create docker

--- main.py
+++ main.py
@@ -5,12 +5,4 @@ from util.excelUtil import ExcelUtil
 from util.OsUtil import OsUtil
 
 if __name__ == '__main__':
-    files = OsUtil.walkFile('C://Users//llllll//Desktop//itsm2')
-    list = []
-    for f in files:
-        f = f.replace('\\', '//')
-        excelData = ExcelUtil.open_excel(f, 'Sheet1')
-        excelData.drop(excelData[pd.isna(excelData['处理类型'])].index, inplace=True)
-        list.append(excelData)
-    df = pd.concat(list)
-    df.to_excel('C://Users//llllll//Desktop//itsm2//6.xlsx', index=False, header=True)
+    print(1111)
